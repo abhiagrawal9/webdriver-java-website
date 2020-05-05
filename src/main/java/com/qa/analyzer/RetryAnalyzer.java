79a41a8d64a40a59512b404838e3ecbacc163b78
AbhishekMM27 <abhishek.agrawal@mutualmobile.com>
2020-05-05 17:13:15 +0530

    implemented retry analyzer logic

---
+++ src/main/java/com/qa/analyzer/RetryAnalyzer.java
@@ -0,0 +1,18 @@
+package com.qa.analyzer;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+
+	int counter = 1, retryLimit = 2;
+
+	@Override
+	public boolean retry(ITestResult result) {
+		if(counter<=retryLimit){
+			counter++;
+			return true;
+		}
+		return false;
+	}
+}
